2e191e2fbe1e563642c3587b67fe5eb592fdef1e
xg85 <xg85@students.waikato.ac.nz>
2024-04-24 20:22:34 +1200

    hash

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from "react";
 import "./App.css";
-import { HashRouter as Router, Routes, Route, Outlet } from "react-router-dom";
+import { HashRouter, Routes, Route, Outlet } from "react-router-dom"; // 注意这里的导入
 import Timeline from "./components/Timeline";
 import Carousel from "./components/Carousel";
 import PhotoGallery from "./components/PhotoGallery";
 
 function App() {
   return (
-    <Router basename={process.env.REACT_APP_BASENAME}>
+    <HashRouter>
+      {" "}
       <div className="App">
         <header className="App-header"></header>
         <Routes>
+          {" "}
           <Route path="/" element={<Layout />} />
-          /* <Route path="/carousel" element={<Carousel />} />
-          <Route path="/photogallery" element={<PhotoGallery />} /> */
+          <Route path="/carousel" element={<Carousel />} />
+          <Route path="/photogallery" element={<PhotoGallery />} />
         </Routes>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
